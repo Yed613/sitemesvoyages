1a8d5bcc41f50b0fb18035fca30b68a27fea9a8e
yedidia <yedidiabinyamincahn@gmail.com>
2024-03-14 13:53:43 +0100

    Commit du projet symfony 2 authentification admin

---
+++ wamp64/www/mesvisites/tests/VisiteTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Tests;
+
+use App\Entity\Visite;
+use App\Entity\Environnement;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of VisiteTest
+ *
+ * @author emds
+ */
+class VisiteTest extends TestCase {
+    
+    public function testGetDatecreationString(){
+        $visite = new Visite();
+        $visite->setDatecreation(new DateTime("2022-04-14"));
+        $this->assertEquals("14/04/2022", $visite->getDatecreationString());
+    }
+    
+    public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
+    }    
+}
